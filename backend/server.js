265adde1506d871bb74fc9696eb70ee82c863775
Sean Louie <sean@Seans-MacBook-Air-2.local>
2025-08-12 13:10:43 -0700

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://rep-it.vercel.app', 'https://rep-it-seans-projects-059cf3f4.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
