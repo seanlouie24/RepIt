59b6057186add5a38428c4b7b7f14cc677e1d326
Sean Louie <sean@Seans-MacBook-Air-2.local>
2025-08-12 21:56:50 -0700

    cors edited 2

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ const app = express();
 
 const corsOptions = {
   origin: [
-    'http://localhost:3000', 
+    'http://localhost:3000',
+    'repitapp.vercel.app',
     'https://rep-it.vercel.app', 
     'https://rep-it-seans-projects-059cf3f4.vercel.app'
   ],
